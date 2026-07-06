aa81ee72c0f446eeb57357d1b4c7b699d2506dca
Asif Iqbal <asfiqbl177@gmail.com>
2026-07-06 16:19:12 +0500

    Complete T044: add accessible skip link

--- src/features/dashboard/components/DashboardLayout.tsx
+++ src/features/dashboard/components/DashboardLayout.tsx
@@ -15,26 +15,37 @@ export function DashboardLayout() {
   const [mobileNavOpen, setMobileNavOpen] = useState(false)
 
   return (
-    <div className="grid h-screen grid-rows-[auto_1fr_auto] overflow-hidden">
-      <Navbar
-        onMenuToggle={() => setMobileNavOpen(true)}
-        isMobile={isMobile}
-      />
-      <div className="grid min-h-0 grid-cols-[auto_minmax(0,1fr)] overflow-hidden">
-        <div className="col-start-1 hidden md:flex">
-          <Sidebar state={sidebarState} onToggle={toggle} />
-        </div>
-        {isMobile && (
-          <MobileNav
-            isOpen={mobileNavOpen}
-            onClose={() => setMobileNavOpen(false)}
-          >
+    <>
+      <a href="#map" className="sr-only focus:not-sr-only">
+        Skip to map
+      </a>
+      <div className="grid h-screen grid-rows-[auto_1fr_auto] overflow-hidden">
+        <Navbar
+          onMenuToggle={() => setMobileNavOpen(true)}
+          isMobile={isMobile}
+        />
+        <div className="grid min-h-0 grid-cols-[auto_minmax(0,1fr)] overflow-hidden">
+          <div className="col-start-1 hidden md:flex">
             <Sidebar state={sidebarState} onToggle={toggle} />
-          </MobileNav>
-        )}
-        <MapContainer className="col-start-2 h-full min-h-0 w-full min-w-0" />
+          </div>
+          {isMobile && (
+            <MobileNav
+              isOpen={mobileNavOpen}
+              onClose={() => setMobileNavOpen(false)}
+            >
+              <Sidebar state={sidebarState} onToggle={toggle} />
+            </MobileNav>
+          )}
+          <div
+            id="map"
+            tabIndex={-1}
+            className="col-start-2 h-full min-h-0 w-full min-w-0"
+          >
+            <MapContainer className="h-full w-full" />
+          </div>
+        </div>
+        <StatusBar />
       </div>
-      <StatusBar />
-    </div>
+    </>
   )
 }
